4d5d2e28d37b0564d25543ac89176085b83c4277
Rukiza <theproros@gmail.com>
2014-09-08 13:01:10 +1200

    Made a room class and a test class for rooms.

---
+++ Game/src/game/world/Room.java
@@ -0,0 +1,5 @@
+package game.world;
+
+public class Room {
+
+}
---
+++ Game/src/test/world/RoomTests.java
@@ -0,0 +1,14 @@
+package test.world;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RoomTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
