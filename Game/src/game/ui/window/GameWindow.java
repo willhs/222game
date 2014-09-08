3755b1014aadf0b93c839779250a6e9f0d0bd03b
Nicky <nickyvanhulst@gmail.com>
2014-09-08 12:57:10 +1200

    added a simple frame.

--- Game/src/game/ui/window/GameWindow.java
+++ Game/src/game/ui/window/GameWindow.java
@@ -1,5 +1,29 @@
 package game.ui.window;
 
-public class GameWindow {
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class GameWindow extends JFrame{
+
+	private static final String title = "Game";
+
+	private static final int WINDOW_SIZE = 100;
+	private static final int FRAME_HEIGHT = WINDOW_SIZE*9;
+	private static final int FRAME_WIDTH = WINDOW_SIZE*16;
+
+	/**
+	 * Constructor for the GameFrame
+	 * */
+	public GameWindow(){
+		super(title);
+
+		//set the size of the frame
+		setResizable(false);
+		setSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+		setLocationRelativeTo(null);
+		setVisible(true);
+
+	}
 
 }
