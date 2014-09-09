5c71f35209775c438fc32598a513038827083cb5
Rukiza <theproros@gmail.com>
2014-09-10 11:46:29 +1200

    Room now implements Place and meets the requirements of place.
    3 lists added to Room, Exits, Players, and items these list represent
    what is in the room. This could include ferniture.

--- Game/src/game/world/model/Room.java
+++ Game/src/game/world/model/Room.java
@@ -1,5 +1,32 @@
 package game.world.model;
 
-public class Room {
+import java.util.*;
 
+public class Room implements Place{
+
+	private final List<Exit> exits;
+	private final List<Player> players;
+	private final List<Item> items;
+	
+	public Room (List<Exit> exits, List<Item> items){
+		this.exits = exits;
+		this.players = new ArrayList<Player>();
+		this.items = items;
+	}
+	
+	@Override
+	public Iterator<Exit> getExits() {
+		return exits.iterator();
+	}
+
+	@Override
+	public List<Item> getItems() {
+		return items;
+	}
+
+	@Override
+	public List<Player> getPlayers() {
+		return players;
+	}
+	
 }
