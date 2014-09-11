5807c7dfd22d79bdaa1755db7f64a65d15bc7439
Rukiza <theproros@gmail.com>
2014-09-11 17:20:02 +1200

    Added some Comments

--- Game/src/game/world/model/Item.java
+++ Game/src/game/world/model/Item.java
@@ -1,5 +1,12 @@
 package game.world.model;
 
+/**
+ * Item
+ * All items need to be some where eather in a place 
+ * or a container or a inventory
+ * @author Shane Brewer
+ *
+ */
 public interface Item {
 
 }
