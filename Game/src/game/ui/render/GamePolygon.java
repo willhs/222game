2a6c9311b29ae871a19357d718789725f9f54477
Will Hardwick-Smith <whardwicksmith@gmail.com>
2014-09-16 13:55:50 +1200

    Added game polygon

---
+++ Game/src/game/ui/render/GamePolygon.java
@@ -0,0 +1,15 @@
+package game.ui.render;
+
+/**
+ * @author hardwiwill
+ * Wrapper class for a java.awt.Polygon so that i... TODO
+ */
+public class GamePolygon implements ZComparable{
+
+	@Override
+	public float getZ() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
