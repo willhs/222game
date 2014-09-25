011af756eeb1bb691e4b62e3c7601b1dddd842a0
Rukiza <theproros@gmail.com>
2014-09-26 11:09:21 +1200

    Changes to the PickUpHandler to check Proximity ....... This error needs to be changed in the Movment Hangler Refactoring may be a option.

--- Game/src/game/world/logic/PickupHandler.java
+++ Game/src/game/world/logic/PickupHandler.java
@@ -8,41 +8,53 @@ import game.world.model.*;
 /**
  * 
  * @author Shane Brewer
- *
+ * 
  */
 public class PickupHandler {
 	/**
 	 * Used by the Game Event handler to have a Player pick up an item.
-	 * @param player - player that is to pick up the item
-	 * @param item - item to be picked up.
-	 * @param place - place the player and the item should be.
+	 * 
+	 * @param player
+	 *            - player that is to pick up the item
+	 * @param item
+	 *            - item to be picked up.
+	 * @param place
+	 *            - place the player and the item should be.
 	 * @return - true if and only if the the player now has the item.
 	 */
-	public static boolean PickupItem(Player player, Item item, Place place){
-		if(!checkPlayers(place, player) && !checkItems(place, item)){
+	public static boolean PickupItem(Player player, Item item, Place place) {
+		if (!checkPlayers(place, player) && !checkItems(place, item)) {
 			return false;
 		}
-		if (!checkProximity(player.getPosition(), item.getPosition())){
+		if (!checkProximity(player.getPosition(),player.getBoundingBox(), item.getPosition(), item.getBoundingBox())) {
 			return false;
 		}
-		if(!item.canPickUp()){
+		if (!item.canPickUp()) {
 			return false;
 		}
 		place.removeItem(item);
-		item.setPosition(new Point3D(0,0,0));
+		item.setPosition(new Point3D(0, 0, 0));
 		player.addItem(item);
 		return true;
 
 	}
 
 	/**
-	 * Makes sure that the item and player are within a certain proximity of each other
-	 * @param pointOne - point of the player.
-	 * @param pointTwo - point of the item.
+	 * Makes sure that the item and player are within a certain proximity of
+	 * each other
+	 * 
+	 * @param pointOne
+	 *            - point of the player.
+	 * @param pointTwo
+	 *            - point of the item.
 	 * @return - true if they are close enough
 	 */
-	private static boolean checkProximity(Point3D pointOne, Point3D pointTwo){
-		if ((Math.abs(pointOne.x -pointTwo.x) <= 5) && (Math.abs(pointOne.z -pointTwo.z) <= 5)){
+	private static boolean checkProximity(Point3D pointOne,
+			Rectangle3D BoundingOne, Point3D pointTwo, Rectangle3D BoundingTwo) {
+		if ((Math.abs(pointOne.x - pointTwo.x) <= 5 + (BoundingOne.getWidth() + BoundingTwo
+				.getWidth()) / 2)
+				&& (Math.abs(pointOne.z - pointTwo.z) <= 5 + (BoundingOne
+						.getLength() + BoundingTwo.getLength()) / 2)) {
 			return true;
 		}
 		return false;
@@ -50,14 +62,17 @@ public class PickupHandler {
 
 	/**
 	 * Checks that the player is in the room
-	 * @param place - place player should be in.
-	 * @param player - player to check.
+	 * 
+	 * @param place
+	 *            - place player should be in.
+	 * @param player
+	 *            - player to check.
 	 * @return - true only if player is in the room.
 	 */
-	private static boolean checkPlayers(Place place, Player player){
+	private static boolean checkPlayers(Place place, Player player) {
 		Iterator<Player> players = place.getPlayers();
-		while(players.hasNext()){
-			if(players.next().equals(player)){
+		while (players.hasNext()) {
+			if (players.next().equals(player)) {
 				return true;
 			}
 		}
@@ -66,14 +81,17 @@ public class PickupHandler {
 
 	/**
 	 * Checks that the item is in the room.
-	 * @param place -place that the item should be.
-	 * @param item - item that should be in the room.
+	 * 
+	 * @param place
+	 *            -place that the item should be.
+	 * @param item
+	 *            - item that should be in the room.
 	 * @return - true only if the item is in the room.
 	 */
-	private static boolean checkItems(Place place, Item item){
+	private static boolean checkItems(Place place, Item item) {
 		Iterator<Item> items = place.getItems();
-		while(items.hasNext()){
-			if(items.next().equals(item)){
+		while (items.hasNext()) {
+			if (items.next().equals(item)) {
 				return true;
 			}
 		}
