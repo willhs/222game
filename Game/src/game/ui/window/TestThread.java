174078c6ee033be6fd08e48d806a1a00ecca4be0
Nicky <nickyvanhulst@gmail.com>
2014-09-26 12:40:39 +1200

    added animation to menus.

---
+++ Game/src/game/ui/window/TestThread.java
@@ -0,0 +1,28 @@
+package game.ui.window;
+
+
+
+public class TestThread extends Thread {
+	private final int delay; // delay between pulses in us
+	private final GameWindow display;
+
+	public TestThread(int delay, GameWindow display) {
+		this.delay = delay;
+		this.display = display;
+	}
+
+	public void run() {
+		while(1 == 1) {
+			// Loop forever
+			try {
+				Thread.sleep(delay);
+				if(display != null) {
+					display.repaint();
+				}
+			} catch(InterruptedException e) {
+				// should never happen
+			}
+		}
+	}
+}
+
