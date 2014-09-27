fed75aaab386fa6e5747bf60f541ecba2f867d3d
Rukiza <theproros@gmail.com>
2014-09-27 15:54:22 +1200

    Pushing to avoid merge issuses later has comments about implementation of the clinet and server world.

--- Game/src/game/world/model/ClientWorld.java
+++ Game/src/game/world/model/ClientWorld.java
@@ -1,5 +1,25 @@
 package game.world.model;
 
 public class ClientWorld extends World{
+	public String getCommand(String action){
+		//where action is like "up", "down", "right", etc
 
+		//get the viewing direction from will's static stuff
+		//returns a single command like played.x += 10 or something
+		//or move player 10 or whatever
+		return null;
+	}
+
+	public boolean applyCommand(String command){
+		//applies command to the world
+		//returns a list of commands that resulted from running the given command
+		//returns an empty string array if the command was invalid or whatever
+
+		//I will call this one by one on the list of commands returned by the server
+		return false;
+	}
+
+	public void replaceCurrentRoom(Room room){
+		//replaces the current room with the given room object
+	}
 }
--- Game/src/game/world/model/ServerWorld.java
+++ Game/src/game/world/model/ServerWorld.java
@@ -1,5 +1,18 @@
 package game.world.model;
 
+import java.util.*;
+
 public class ServerWorld extends World{
 
+	public List<String> applyCommand(String command){
+		//applies command to the world
+		//returns a list of commands that resulted from running the given command
+		//returns an empty string array if the command was invalid or whatever
+		return null;
+	}
+
+	public Room getRoomPlayerIsIn(Player p){
+		//gets the room the given player is in
+		return null;
+	}
 }
