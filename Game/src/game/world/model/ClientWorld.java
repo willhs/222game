cf083d66b5b57f065b4b62d224a889af79cc7620
Rukiza <theproros@gmail.com>
2014-09-27 18:13:21 +1200

    Made it so that a player can be inishilized on client side, to the server and back.

--- Game/src/game/world/model/ClientWorld.java
+++ Game/src/game/world/model/ClientWorld.java
@@ -1,48 +1,71 @@
 package game.world.model;
 
 import game.ui.render.util.Transform;
+import game.world.dimensions.Point3D;
 
 import java.util.*;
 
-public abstract class ClientWorld extends ServerWorld{
+public abstract class ClientWorld extends ServerWorld {
 
 	private float movmentScaler = 10.0f;
 	private HashMap<String, Transform> keyPressToDirection;
 	private Player clientsPlayer;
 
-	public ClientWorld(){
+	public ClientWorld() {
 		keyPressToDirection = new HashMap<String, Transform>();
-		keyPressToDirection.put("Right", Transform.newYRotation((float)(Math.PI/2.0f)));
+		keyPressToDirection.put("Right",
+				Transform.newYRotation((float) (Math.PI / 2.0f)));
 		keyPressToDirection.put("Up", Transform.newYRotation(0));
-		keyPressToDirection.put("Left", Transform.newYRotation((float)-(Math.PI/2.0f)));
-		keyPressToDirection.put("Down", Transform.newYRotation((float)(Math.PI)));
+		keyPressToDirection.put("Left",
+				Transform.newYRotation((float) -(Math.PI / 2.0f)));
+		keyPressToDirection.put("Down",
+				Transform.newYRotation((float) (Math.PI)));
 	}
 
-	public String getCommand(String action){
-		//where action is like "up", "down", "right", etc
+	public String getCommand(String action) {
+		// where action is like "up", "down", "right", etc
 
-		//get the viewing direction from will's static stuff
-		//returns a single command like played.x += 10 or something
-		//or move player 10 or whatever
+		// get the viewing direction from will's static stuff
+		// returns a single command like played.x += 10 or something
+		// or move player 10 or whatever
 		return null;
 	}
 
-	public List<String> applyCommand(String command){
+	public List<String> applyCommand(String command) {
 		List<String> commandList = super.applyCommand(command);
-		//applies command to the world
-		//returns a list of commands that resulted from running the given command
-		//returns an empty string array if the command was invalid or whatever
+		Scanner scan = new Scanner(command);
+		if (scan.hasNext("ClientPlayerPlacement")) {
+			setClientPlayer(scan);
+		}
+		// applies command to the world
+		// returns a list of commands that resulted from running the given
+		// command
+		// returns an empty string array if the command was invalid or whatever
 
-		//I will call this one by one on the list of commands returned by the server
-		return null;
+		// I will call this one by one on the list of commands returned by the
+		// server
+		return new ArrayList<String>();
 	}
 
-	public void replaceCurrentRoom(Room room){
-		//replaces the current room with the given room object
+	public void replaceCurrentRoom(Room room) {
+		// replaces the current room with the given room object
 	}
 
-	public String getSetClientPlayer(Player player){
+	public String getSetClientPlayer(Player player) {
 		clientsPlayer = player;
-		return "ServerPlayerPlacement Name ( "+player.name+" ) BoundingBox ( "+player.getBoundingBox()+" )";
+		return "ServerPlayerPlacement Name ( " + player.name
+				+ " ) BoundingBox ( " + player.getBoundingBox() + " )";
 	}
+
+	private boolean setClientPlayer(Scanner scan) {
+		while (!scan.hasNext("Position")) {
+			scan.next();
+		}
+		Point3D position = parsePosition(scan);
+		Place place = getStartPlace();
+		clientsPlayer.move(position);
+		place.addPlayer(clientsPlayer);
+		return true;
+	}
+
 }
