1cabd27de79a823556df6011aca50e0e06ca42bb
Rukiza <theproros@gmail.com>
2014-09-27 16:07:34 +1200

    Changes made to hiracy of the World class ....  this will help the game work better.

--- Game/src/game/world/model/ClientWorld.java
+++ Game/src/game/world/model/ClientWorld.java
@@ -1,6 +1,8 @@
 package game.world.model;
 
-public class ClientWorld extends World{
+import java.util.*;
+
+public abstract class ClientWorld extends ServerWorld{
 	public String getCommand(String action){
 		//where action is like "up", "down", "right", etc
 
@@ -10,16 +12,18 @@ public class ClientWorld extends World{
 		return null;
 	}
 
-	public boolean applyCommand(String command){
+	public List<String> applyCommand(String command){
 		//applies command to the world
 		//returns a list of commands that resulted from running the given command
 		//returns an empty string array if the command was invalid or whatever
 
 		//I will call this one by one on the list of commands returned by the server
-		return false;
+		return null;
 	}
 
 	public void replaceCurrentRoom(Room room){
 		//replaces the current room with the given room object
 	}
+
+
 }
--- Game/src/game/world/model/ServerWorld.java
+++ Game/src/game/world/model/ServerWorld.java
@@ -2,7 +2,7 @@ package game.world.model;
 
 import java.util.*;
 
-public class ServerWorld extends World{
+public abstract class ServerWorld {
 
 	public List<String> applyCommand(String command){
 		//applies command to the world
@@ -11,8 +11,35 @@ public class ServerWorld extends World{
 		return null;
 	}
 
-	public Room getRoomPlayerIsIn(Player p){
-		//gets the room the given player is in
-		return null;
-	}
+	/**
+	 * getPlaces method returns all the places in the form
+	 * of a iterator
+	 * @return - iterator over the list of places.
+	 */
+	public abstract Iterator<Place> getPlaces();
+
+	/**
+	 * getPlayers method returns all the players that are
+	 * in the list in the form of a iterator
+	 * @return
+	 */
+	public abstract Iterator<Player> getPlayers();
+
+	/**
+	 * This method will return the place that the player is currently
+	 * in.
+	 * @param player - the player that will be used to find the room.
+	 * @return -returns the place the player is currently.
+	 */
+	public abstract Place getPlaceOfPlayer(Player player);
+
+	/**
+	 * This method will return a iterator of players in a place.
+	 * not really needed if you have the place but added.
+	 * @param place - place that the players are in.
+	 * @return - returns the list of players in that place.
+	 */
+	public abstract Iterator<Player> getPlayerOfPlace(Place place);
+
+	public abstract void addPlayerToGameWorld(Player player);
 }
