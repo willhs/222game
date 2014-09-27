6c0d3ce462eedbe42565d3e450a7b007061b0cde
Rukiza <theproros@gmail.com>
2014-09-27 12:28:07 +1200

    Made some changes to the room class to alowe the player to be set even when the room is weird.

--- Game/src/test/world/util/SingleRoomWorldTest.java
+++ Game/src/test/world/util/SingleRoomWorldTest.java
@@ -31,8 +31,8 @@ public class SingleRoomWorldTest {
 	public SingleRoomWorldTest(){
 		//int[] xpoints = new int[]{0,200, 200,400,400,0};
 		//int[] ypoints = new int[]{0,0,   200,200,400,400};
-		int[] xpoints = new int[]{0, 400, 400, 0};
-		int[] ypoints = new int[]{0, 0, 400, 400};
+		int[] xpoints = new int[]{200, 400, 400, 200};
+		int[] ypoints = new int[]{200, 200, 400, 400};
 		Polygon p = new Polygon(xpoints, ypoints, xpoints.length);
 		List<Item> items = new ArrayList<Item>();
 		items.add(new Table("Table1", new Point3D(250, 0+Trixel.SIZE/2, 250), new Rectangle3D(50, 50, 50)));
