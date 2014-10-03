347f0b733bd3f82a759950979c28467d210ed890
Will Hardwick-Smith <whardwicksmith@gmail.com>
2014-10-03 15:15:47 +1300

    Level Maker can choose trixel colour and erase trixels

--- Game/src/game/ui/render/levelmaker/LevelMakerGUI.java
+++ Game/src/game/ui/render/levelmaker/LevelMakerGUI.java
@@ -4,6 +4,7 @@ import game.ui.render.Renderer;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,6 +12,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JPanel;
 
 public class LevelMakerGUI extends JPanel{
@@ -29,13 +31,36 @@ public class LevelMakerGUI extends JPanel{
 		setBackground(Color.BLACK);
 		setLayout(new BorderLayout());
 
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		add(buttonPanel, BorderLayout.NORTH);
+
+		JButton colourButton = new JButton("Colour");
+		colourButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				levelMaker.setColour(JColorChooser.showDialog(null, "choose colour", Color.WHITE));
+			}
+		});
+		buttonPanel.add(colourButton);
+
+		JButton eraserButton = new JButton("Eraser");
+		eraserButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				levelMaker.toggleEraser();
+				System.out.println("eraser mode: "+levelMaker.getIsEraserModeOn());
+			}
+		});
+		buttonPanel.add(eraserButton);
+
 		JButton saveButton = new JButton("Save");
 		saveButton.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				levelMaker.writeTrixelsToFile();
 			}
 		});
-		add(saveButton, BorderLayout.NORTH);
+		buttonPanel.add(saveButton);
 	}
 
 	private void dealWithMouseDragged(int dx, int dy) {
@@ -46,10 +71,10 @@ public class LevelMakerGUI extends JPanel{
 	@Override
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		long start = System.currentTimeMillis();
+		//long start = System.currentTimeMillis();
 		Renderer.renderTrixels(g, levelMaker.getTrixels(), levelMaker.getLastTransform());
-		System.out.printf("time taken to transform and draw trixels: %d\n",
-				System.currentTimeMillis()-start);
+		/*System.out.printf("time taken to transform and draw trixels: %d\n",
+				System.currentTimeMillis()-start);*/
 	}
 
 	public class WillMouseMotionListener extends MouseAdapter{
@@ -76,7 +101,7 @@ public class LevelMakerGUI extends JPanel{
 
 		@Override
 		public void mouseClicked(MouseEvent e){
-			levelMaker.attemptCreateTrixel(e.getX(), e.getY());
+			levelMaker.dealWithMouseClick(e.getX(), e.getY());
 			repaint();
 		}
 	}
